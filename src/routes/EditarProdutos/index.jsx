bee86aea6ac5389d47a305509822f59ef4502fcc
davidbryan20 <davidbryan20@icloud.com>
2023-09-15 07:42:00 -0300

    Corrigindo erros part2

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,1 +1,9 @@
+export default function EditarProddutos() {
+  return (
 
+    <main>
+        <h1>EditarProddutos</h1>
+    </main>
+
+  )
+}
---
+++ src/routes/Error/index.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <>
+        <h1>Erro 404 - Pãgina encontrada</h1>
+    </>
+  )
+}
